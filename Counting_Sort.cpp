a47cb229d1f5fda2af2f9e17f794ee75405181f2
billygrahan <billygrahan@alu.ufc.br>
2025-07-21 22:51:19 -0300

    ajuste no counting e implementado o Radix!

--- Counting_Sort.cpp
+++ Counting_Sort.cpp
@@ -6,31 +6,39 @@
 using namespace std;
 using namespace std::chrono;
 
-vector<int> countingSort(const vector<int>& A, int k) {
+vector<int> countingSort(const vector<int> &A, int k)
+{
     int n = A.size();
     vector<int> Saida(n);
-    vector<int> Count(k + 1, 0); 
+    vector<int> Count(k + 1, 0);
 
-    for (int j = 0; j < n; j++) {
-        Count[A[j]] = Count[A[j]] + 1;
+    for (int j = 0; j < n; j++)
+    {
+        Count[A[j]] += 1;
     }
 
-    for (int i = 1; i <= k; i++) {
-        Count[i] = Count[i] + Count[i - 1];
+    for (int i = 1; i <= k; i++)
+    {
+        Count[i] += Count[i - 1];
     }
 
-    for (int j = n - 1; j >= 0; j--) {
+    for (int j = n - 1; j >= 0; j--)
+    {
         Saida[Count[A[j]] - 1] = A[j];
-        Count[A[j]] = Count[A[j]] - 1;
+        Count[A[j]] -= 1;
     }
 
     return Saida;
 }
 
-void orderedArray(vector<int>& arr) {
-    cout << endl  << endl << "-------------------------------------------------------------------"<< endl;
+void orderedArray(vector<int> &arr)
+{
+    cout << endl
+         << endl
+         << "-------------------------------------------------------------------" << endl;
     cout << "arrey: ";
-    for (int i = 0; i < arr.size(); ++i) {
+    for (int i = 0; i < arr.size(); ++i)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
@@ -41,19 +49,23 @@ void orderedArray(vector<int>& arr) {
 
     // Fim da medição
     auto end = high_resolution_clock::now();
-    double duration = duration_cast<std::chrono::duration<double, std::milli>>(end - start).count();
+    double duration = duration_cast<std::chrono::duration<double, std::milli> >(end - start).count();
     cout.precision(10);
     cout << "Tempo de execução: " << fixed << duration << " milissegundos" << endl;
 
     cout << "array Ordenado: ";
-    for (int i = 0; i < arr.size(); ++i) {
+    for (int i = 0; i < arr.size(); ++i)
+    {
         cout << arr[i] << " ";
     }
-    cout << endl << "-------------------------------------------------------------------" << endl << endl;
+    cout << endl
+         << "-------------------------------------------------------------------" << endl
+         << endl;
 }
 
-int main() {
-    vector<int> arr = {1,2,5,4,0,0,2,6,4,6};
+int main()
+{
+    vector<int> arr = {1, 2, 5, 4, 0, 0, 2, 6, 4, 6};
 
     orderedArray(arr);
 
--- Radix_Sort.cpp
+++ Radix_Sort.cpp
@@ -1,69 +1,105 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <algorithm>
 
 using namespace std;
 using namespace std::chrono;
 
-void orderedArray(vector<int>& arr) {
-    cout << endl  << endl << "-------------------------------------------------------------------"<< endl;
+void countingSortRadix(vector<int> &arr, int exp)
+{
+    int n = arr.size();
+    vector<int> Saida(n);
+    vector<int> count(10, 0);
+
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    for (int k = 1; k < 10; k++)
+        count[k] += count[k - 1];
+
+    for (int l = n - 1; l >= 0; l--)
+    {
+        int digito = (arr[l] / exp) % 10;
+        Saida[count[digito] - 1] = arr[l];
+        count[digito]--;
+    }
+
+    for (int m = 0; m < n; m++)
+        arr[m] = Saida[m];
+}
+
+// Radix Sort principal
+void radixSort(vector<int> &arr)
+{
+    int maxVal = *max_element(arr.begin(), arr.end());
+
+    for (int exp = 1; maxVal / exp > 0; exp *= 10)
+        countingSortRadix(arr, exp);
+}
+
+void orderedArray(vector<int> &arr)
+{
+    cout << endl
+         << endl
+         << "-------------------------------------------------------------------" << endl;
     cout << "arrey: ";
-    for (int i = 0; i < arr.size(); ++i) {
+    for (int i = 0; i < arr.size(); ++i)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
 
     auto start = high_resolution_clock::now();
 
-    //insertionSort(arr);
+    radixSort(arr);
 
     // Fim da medição
     auto end = high_resolution_clock::now();
-    double duration = duration_cast<std::chrono::duration<double, std::milli>>(end - start).count();
+    double duration = duration_cast<std::chrono::duration<double, std::milli> >(end - start).count();
     cout.precision(10);
     cout << "Tempo de execução: " << fixed << duration << " milissegundos" << endl;
 
     cout << "array Ordenado: ";
-    for (int i = 0; i < arr.size(); ++i) {
+    for (int i = 0; i < arr.size(); ++i)
+    {
         cout << arr[i] << " ";
     }
-    cout << endl << "-------------------------------------------------------------------" << endl << endl;
+    cout << endl
+         << "-------------------------------------------------------------------" << endl
+         << endl;
 }
 
-void orderedArray_char(vector<char>& arr) {
-    cout << endl  << endl << "-------------------------------------------------------------------"<< endl;
-    cout << "arrey: ";
-    for (int i = 0; i < arr.size(); ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+// void orderedArray_char(vector<char>& arr) {
+//     cout << endl  << endl << "-------------------------------------------------------------------"<< endl;
+//     cout << "arrey: ";
+//     for (int i = 0; i < arr.size(); ++i) {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
 
-    auto start = high_resolution_clock::now();
+//     auto start = high_resolution_clock::now();
 
-    //insertionSort_chat(arr);
+//     //insertionSort_chat(arr);
 
-    // Fim da medição
-    auto end = high_resolution_clock::now();
-    double duration = duration_cast<std::chrono::duration<double, std::milli>>(end - start).count();
-    cout.precision(10);
-    cout << "Tempo de execução: " << fixed << duration << " milissegundos" << endl;
+//     // Fim da medição
+//     auto end = high_resolution_clock::now();
+//     double duration = duration_cast<std::chrono::duration<double, std::milli>>(end - start).count();
+//     cout.precision(10);
+//     cout << "Tempo de execução: " << fixed << duration << " milissegundos" << endl;
 
-    cout << "array Ordenado: ";
-    for (int i = 0; i < arr.size(); ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl << "-------------------------------------------------------------------" << endl << endl;
-}
+//     cout << "array Ordenado: ";
+//     for (int i = 0; i < arr.size(); ++i) {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl << "-------------------------------------------------------------------" << endl << endl;
+// }
 
-int main() {
-    vector<int> arr1 = {34, 9, 82, 9, -81, 71, 12, -59, -83, -28};
-    vector<char> arr2 = {'b', 'i', 'l', 'l', 'y', 'g', 'r', 'a', 'h', 'a', 'n'};
-    vector<int> arr3 = {86, 49, 28, 72, 63, 38, 31, 89, 24, 29};
-    vector<int> arr4 = {81, 20, 55, 17, 24, 32, 14, 13, 68, 20};
-    vector<int> arr5 = {1,2,5,4,0,0,2,6,4,6};
+int main()
+{
     vector<int> arr6 = {49230, 89952, 25237, 91194, 97087, 48569, 16673, 45849, 38870, 13701};
 
-    orderedArray_char(arr2);
+    orderedArray(arr6);
 
     return 0;
 }
